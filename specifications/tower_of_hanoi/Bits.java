26870a05672aa00a6b329d7961dda8d7199f2ade
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2019-03-12 16:24:27 -0700

    Leave note about TLC refactoring that moved Value impl classes to subpackage.

--- specifications/tower_of_hanoi/Bits.java
+++ specifications/tower_of_hanoi/Bits.java
@@ -1,5 +1,11 @@
 import tlc2.value.IntValue;
 
+// For TLC 2.14 (shipped as part of Toolbox 1.5.8) the correct
+// package for IntValue (and all value implementation classes)
+// will be tlc2.value.impl.  In other words, the import above
+// has to be replaced with:
+//import tlc2.value.impl.IntValue;
+
 public class Bits {
     public static IntValue And(IntValue x, IntValue y) {
         return IntValue.gen(x.val & y.val);
